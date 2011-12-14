ccbac36f1c2fc2afc63895489066aa4b0b963d53
wgyouree@gmail.com <wgyouree@gmail.com@1fae5450-16ca-7d49-7bc0-ac6fe2e28e1a>
2011-12-14 20:34:38 +0000

    - factored out java process execution into AbstractNode class

--- trunk/src/edu/gatech/cs7210/p2pmapreduce/node/SlaveNode.java
+++ trunk/src/edu/gatech/cs7210/p2pmapreduce/node/SlaveNode.java
@@ -1,16 +1,10 @@
 package edu.gatech.cs7210.p2pmapreduce.node;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import de.uniba.wiai.lspi.chord.data.URL;
 import edu.gatech.cs7210.p2pmapreduce.ApplicationContext;
 import edu.gatech.cs7210.p2pmapreduce.task.ITask;
 
-public class SlaveNode implements INode {
+public class SlaveNode extends AbstractNode {
 
 	public enum SlaveType {
 		DATA_NODE, TASK_TRACKER
@@ -27,23 +21,8 @@ public class SlaveNode implements INode {
 	}
 	
 	public boolean run() {
-		try {
-			Process p = Runtime.getRuntime().exec(
-					ApplicationContext.getInstance().getBinDir() + File.separator + "slaves.sh");
-			InputStream s = p.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(s));
-			String line = reader.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = reader.readLine();
-			}
-			return true;
-		} catch (IOException e) {
-			System.err.println("Failed to run slave");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-		return false;
+		return executeCommand(
+				ApplicationContext.getInstance().getBinDir() + File.separator + "slaves.sh");
 	}
 	
 	public boolean update(URL url) {
